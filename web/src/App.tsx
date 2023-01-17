d34c473d9bdc305e8fe9611f8023a8c71d500b7b
Juniel Filappi <juniel123123@outlook.com>
2023-01-16 20:36:36 -0400

    refactor: alter function findValueInObject

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Habit } from "./components/Habit";
+
+function App() {
+  return (
+    <div>
+      <Habit completed={5} />
+      <Habit completed={10} />
+      <Habit completed={15} />
+      <Habit completed={20} />
+    </div>
+  );
+}
+
+export default App;
